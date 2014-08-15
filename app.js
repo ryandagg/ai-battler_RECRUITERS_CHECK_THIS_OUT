0d960f1b965adf3cd76e99df897e4ccaace7afa8
ryandagg <ryan.dagg@gmail.com>
2014-08-15 17:11:51 -0600

    minor

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.get('/random-battle', indexController.randomBattle);
 app.get('/solo', indexController.solo);
 app.get('/create-team', indexController.createTeam);
 
-
-var server = app.listen(7342, function() {
+var port = Number(process.env.PORT || 5000);
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
+
